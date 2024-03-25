971b0bdddebe730e04b8af0dc83a0d8dccf9af7e
LukynkaCZE <jiraklukas058@seznam.cz>
2024-03-25 19:59:29 +0100

    Add Tests

--- src/test/kotlin/BasicGetTest.kt
+++ src/test/kotlin/BasicGetTest.kt
@@ -1,37 +1,51 @@
 import org.junit.jupiter.api.Test
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.io.File
 import kotlin.test.assertEquals
 
 class BasicGetTest {
-
-    private var client: HttpClient = HttpClient.newHttpClient()
-
-    private lateinit var request: HttpRequest
-    private lateinit var response: HttpResponse<String>
-
     init {
         TestServer
-        request = HttpRequest.newBuilder()
-            .uri(tUri("/basic"))
-            .build()
-
-        response = client.send(request, HttpResponse.BodyHandlers.ofString())
     }
 
     @Test
     fun `test status code`() {
+        val response = testRequest("/statusCode")
         assertEquals(200, response.statusCode())
     }
 
     @Test
     fun `test response`() {
+        val response = testRequest("/response")
         assertEquals("hello world", response.body())
     }
 
     @Test
     fun `test url parameters`() {
-        
+        val response = testRequest("/urlparams/test123/storage/test456")
+        assertEquals("test123,test456", response.body())
+    }
+
+    @Test
+    fun `test query parameters`() {
+        val response = testRequest("/query?client_token=abcd123456&state=logged_in")
+        assertEquals("abcd123456,logged_in", response.body())
+    }
+
+    @Test
+    fun `test request headers`() {
+        val response = testRequest("/headers/")
+        assertEquals(response.headers().map().contains("Authorization"), true)
+    }
+
+    @Test
+    fun `test image`() {
+        val response = testRequest("/image/")
+        assertEquals(response.body(), File("src/test/imgs/test.png").readText())
+    }
+
+    @Test
+    fun `test json`() {
+        val response = testRequest("/json/")
+        assertEquals(response.body(), File("src/test/test.json").readText())
     }
 }
--- src/test/kotlin/TestUtils.kt
+++ src/test/kotlin/TestUtils.kt
@@ -1,6 +1,21 @@
 import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 fun tUri(string: String): URI {
     val endpoint = if(string.startsWith("/")) string.removeSuffix("/") else string
     return URI.create("http://127.0.0.1:6900$endpoint")
 }
+
+
+fun testRequest(url: String, body: String? = null): HttpResponse<String> {
+    val client: HttpClient = HttpClient.newHttpClient()
+
+    val request = HttpRequest.newBuilder()
+        .uri(tUri(url))
+        .build()
+
+    return client.send(request, HttpResponse.BodyHandlers.ofString())
+}
+
