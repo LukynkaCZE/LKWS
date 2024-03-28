15f8d84f79523a8dde67d4db340d657e4cd79a81
LukynkaCZE <jiraklukas058@seznam.cz>
2024-03-28 21:10:51 +0100

    hi :3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,3 @@
-import java.io.File
-
 fun main(args: Array<String>) {
-    val server = LightweightWebServer(7270)
-
-    server.get("/buy/{product}") { res ->
-        val price = res.queryParameters["price"]?.toInt()
-        val currency = res.queryParameters["currency"]
-        val product = res.parameters["product"]
-
-        val headers = res.requestHeaders["User-agent"]
-
-        val cookies = res.requestCookies
-        res.respond("You bought $product for ${price}${currency}\nUser-agent: $headers\nYour token is (plz dont steal): ${cookies["token"]}")
-    }
-
-    server.get("/gay") {
-        val file = File("./imgs/test.png")
-        it.respondFile(file)
-    }
+    //hello person reading this. I hope you are having a nice day <3
 }
